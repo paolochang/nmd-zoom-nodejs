e592e7db9dc79cbd15225f1c7c3b847e0d477f35
Paolo Chang <chanhwan.chang@gmail.com>
2021-08-19 14:40:25 -0400

    SocketIO: Add Admin UI

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 // import WebSocket from "ws";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
@@ -17,7 +18,16 @@ app.get("/*", (req, res) => res.redirect("/"));
 const serverListener = () => console.log("Listening on http://localhost:3000");
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+});
 
 function findPublicRooms() {
   const { sids, rooms } = wsServer.sockets.adapter;
