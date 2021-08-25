c8bc576f1dcee98a4296b3c59f25c1ccef8b9720
Paolo Chang <chanhwan.chang@gmail.com>
2021-08-24 23:44:57 -0400

    Update ActionBar icon color

--- client/src/components/ActionBar/ActionBar.tsx
+++ client/src/components/ActionBar/ActionBar.tsx
@@ -19,20 +19,20 @@ const ActionBar: React.FC = () => {
     <div className="action-container">
       {isCameraOff ? (
         <button className="action-button" onClick={onClickCamera}>
-          <IoVideocam className="react-icon" size="2.2em" />
+          <IoVideocam className="react-icon" color="limegreen" size="2.2em" />
         </button>
       ) : (
         <button className="action-button" onClick={onClickCamera}>
-          <IoVideocamOff className="react-icon" size="2.2em" />
+          <IoVideocamOff className="react-icon" color="tomato" size="2.2em" />
         </button>
       )}
       {isMute ? (
         <button className="action-button" onClick={onClickSpeaker}>
-          <GiSpeaker className="react-icon" size="2.5em" />
+          <GiSpeaker className="react-icon" color="limegreen" size="2.5em" />
         </button>
       ) : (
         <button className="action-button" onClick={onClickSpeaker}>
-          <GiSpeakerOff className="react-icon" size="2.5em" />
+          <GiSpeakerOff className="react-icon" color="tomato" size="2.5em" />
         </button>
       )}
     </div>
